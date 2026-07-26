366864a2b505d55be022fe1ad27b071578d7571a
Prince soni <157681120+Prince671@users.noreply.github.com>
2026-07-26 21:25:02 +0530

    Time: 2 ms (43.35%), Space: 43.4 MB (43.54%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,12 +1,16 @@
-class Solution {
-    public boolean rotateString(String s, String goal) {
-
-        if (s.length() != goal.length()) {
-            return false;
-        }
-
-        String concatenatedString = s + s;
-
-        return concatenatedString.contains(goal);
-    }
+class Solution { 
+    public boolean rotateString(String s, String goal) { 
+        if (s.length() != goal.length()) { 
+            return false; 
+        } 
+        
+        String concatinatedString = s + s; 
+        
+        
+        if (concatinatedString.contains(goal)) { 
+            return true; 
+        } 
+        
+        return false; 
+    } 
 }
