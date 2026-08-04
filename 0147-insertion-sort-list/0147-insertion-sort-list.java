80a6641ce1bf4e8b792e7009dbc782a5bdf4f8ca
Prince soni <157681120+Prince671@users.noreply.github.com>
2026-08-04 19:20:38 +0530

    Time: 5 ms (89.55%), Space: 46.8 MB (7.26%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -1,47 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
 class Solution {
-
     public ListNode insertionSortList(ListNode head) {
 
-        if (head == null || head.next == null) {
-            return head;
-        }
+        List<Integer> a = new ArrayList<>();
 
         ListNode current = head;
 
         while (current != null) {
-
-            // Assume current node is the minimum
-            ListNode minNode = current;
-
-            // Find the minimum node in the remaining list
-            ListNode compare = current.next;
-
-            while (compare != null) {
-
-                if (compare.val < minNode.val) {
-                    minNode = compare;
-                }
-
-                compare = compare.next;
-            }
-
-            // Swap values
-            int temp = current.val;
-            current.val = minNode.val;
-            minNode.val = temp;
-
+            a.add(current.val);
             current = current.next;
+        }
+
+        Collections.sort(a);
+
+        ListNode temp = head;
+        int i = 0;
+
+        while (temp != null) {
+            temp.val = a.get(i);
+            temp = temp.next;
+            i++;
         }
 
         return head;
