5877fc4abf9a68e48ba1eba45bc728d497c1c36e
Prince soni <157681120+Prince671@users.noreply.github.com>
2026-08-10 16:33:02 +0530

    Time: 1 ms (99.89%), Space: 45.6 MB (32.9%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int compress(char[] chars) {
+        int readIndex=0;
+        int writeIndex=0;
+        while(readIndex<chars.length){
+            char currentChar=chars[readIndex];
+            int count=0;
+            while(readIndex<chars.length && currentChar==chars[readIndex]){
+                count++;
+                readIndex++;
+            }
+            chars[writeIndex]=currentChar;
+            writeIndex++;
+            if(count>1){
+                String countStr=String.valueOf(count);
+                for(char digit:countStr.toCharArray()){
+                    chars[writeIndex]=digit;
+                    writeIndex++;
+                }
+            }
+        }
+        return writeIndex;
+   }
+}
