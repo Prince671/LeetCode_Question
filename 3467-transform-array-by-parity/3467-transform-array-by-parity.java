b5bde4f0b6057e74d7d399911462115f69a4a229
Prince soni <157681120+Prince671@users.noreply.github.com>
2026-08-13 22:24:44 +0530

    Time: 1 ms (99.75%), Space: 46.8 MB (73.06%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.java
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -1,20 +1,27 @@
 class Solution {
     public int[] transformArray(int[] nums) {
-        int n=nums.length;
-        int k=0;
-        int ans[]=new int[n];
-        for(int i=0; i<n; i++){
-            if(nums[i]%2==0){
-                nums[i]=0;
-                ans[k]=nums[i];
-                
-            }else{
-                nums[i]=1;
-                ans[k]=nums[i];
+
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int zeroCount = 0;
+
+        // Count even numbers
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                zeroCount++;
             }
-            k++;
         }
-        Arrays.sort(ans);
+
+        // Fill 0s and 1s
+        for (int i = 0; i < n; i++) {
+            if (i < zeroCount) {
+                ans[i] = 0;
+            } else {
+                ans[i] = 1;
+            }
+        }
+
         return ans;
     }
 }
