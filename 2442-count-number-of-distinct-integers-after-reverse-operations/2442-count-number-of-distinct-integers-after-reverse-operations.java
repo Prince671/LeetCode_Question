01529fd789b206a772c3daa3b3e6be7d4b05cdb0
Prince soni <157681120+Prince671@users.noreply.github.com>
2026-08-18 00:28:19 +0530

    Time: 55 ms (81.47%), Space: 105.6 MB (46.49%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
@@ -1,23 +1,24 @@
 class Solution {
     public int countDistinctIntegers(int[] nums) {
-        ArrayList<Integer> allValues=new ArrayList<>();
-        for(int val:nums){
-            allValues.add(val);
-        }
-        for(int i=0; i<nums.length; i++){
-            int num=nums[i];
-            int reversed=0;
-            while(num>0){
-                int digit=num%10;
-                reversed=(reversed*10)+digit;
-                num/=10;
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+
+            set.add(num);
+
+            int reversed = 0;
+            int temp = num;
+
+            while (temp > 0) {
+                int digit=temp%10;
+                reversed = reversed * 10 + digit;
+                temp /= 10;
             }
-            allValues.add(reversed);
+
+            set.add(reversed);
         }
-        Set<Integer> st=new HashSet<>();
-        for(int val:allValues){
-            st.add(val);
-        }
-        return st.size();
+
+        return set.size();
     }
 }
