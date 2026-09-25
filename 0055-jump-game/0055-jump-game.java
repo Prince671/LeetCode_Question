23e1f098849aae16edf5fbd3e3337a9a366ea7d2
Prince soni <157681120+Prince671@users.noreply.github.com>
2026-09-25 17:24:23 +0530

    Time: 299 ms (7.36%), Space: 47.9 MB (42.39%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,45 +2,45 @@ class Solution {
 
     static boolean isPossible(int[] nums, int index, Boolean[] dp) {
 
+        // Reached the last index
         if (index >= nums.length - 1) {
             return true;
         }
 
+        // Cannot move forward
         if (nums[index] == 0) {
             return false;
         }
 
+        // Already calculated
         if (dp[index] != null) {
             return dp[index];
         }
 
         int maxJump = nums[index];
-        boolean overAllAns = false;
 
         for (int i = 1; i <= maxJump; i++) {
 
             boolean recursionAns =
                 isPossible(nums, index + i, dp);
 
-            overAllAns = overAllAns || recursionAns;
-
-            // Stop as soon as a valid path is found
-            if (overAllAns) {
-                break;
+            // If any path reaches the end
+            if (recursionAns) {
+                dp[index] = true;
+                return true;
             }
         }
 
-        dp[index] = overAllAns;
+        // No path can reach the end
+        dp[index] = false;
 
-        return dp[index];
+        return false;
     }
 
     public boolean canJump(int[] nums) {
 
-        int index = 0;
-
         Boolean[] dp = new Boolean[nums.length];
 
-        return isPossible(nums, index, dp);
+        return isPossible(nums, 0, dp);
     }
 }
