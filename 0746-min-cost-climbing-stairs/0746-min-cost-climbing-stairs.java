10eea6a5222f627d0aeaa9cfccd0ac2911166e8e
Prince soni <157681120+Prince671@users.noreply.github.com>
2026-09-25 12:25:47 +0530

    Time: 1 ms (40.16%), Space: 45 MB (23.31%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public static int solve(int[] cost, int n, int[] dp) {
+
+        if (n == 0 || n == 1) {
+            return cost[n];
+        }
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        dp[n]= cost[n] + Math.min(
+            solve(cost, n - 1, dp),
+            solve(cost, n - 2, dp)
+        );
+
+        return dp[n];
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+
+        int n = cost.length;
+        int dp[]=new int[cost.length];
+    Arrays.fill(dp, -1);
+        return Math.min(
+            solve(cost, n - 1 ,dp),
+            solve(cost, n - 2, dp)
+        );
+    }
+}
